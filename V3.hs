209de9b8cc3e292a4bc303182e0ef6d1782b29f3
Michael Eggers <eggers@hm.edu>
2018-11-06 16:42:30 +0100

    using Float for vectors instead of type variable for V3

--- V3.hs
+++ V3.hs
@@ -1,20 +1,20 @@
-data V3 a = V3 a a a deriving (Show)  
+data V3 = V3 Float Float Float deriving (Show)  
   
-vadd :: (Floating t) => V3 t -> V3 t -> V3 t  
+vadd :: V3 -> V3 -> V3
 (V3 x y z) `vadd` (V3 x2 y2 z2) = V3 (x+x2) (y+y2) (z+z2)
 
-vmult :: (Floating t) => t -> V3 t -> V3 t
+vmult :: Float -> V3 -> V3
 (s) `vmult` (V3 x y z) = V3 (s*x) (s*y) (s*z)
 
-vdiv :: (Floating t) => V3 t -> t -> V3 t
+vdiv ::  V3 -> Float -> V3
 (V3 x y z) `vdiv` (s) = V3 (x/s) (y/s) (z/s)
 
-dot :: (Floating t) => V3 t -> V3 t -> t
+dot :: V3 -> V3 -> Float
 (V3 x y z) `dot` (V3 x2 y2 z2) = (x*x2 + y*y2 + z*z2)
 
-vlength :: (Floating t) => V3 t -> t
+vlength ::V3 -> Float
 vlength (V3 x y z) = (sqrt (dot (V3 x y z) (V3 x y z)))
 
-normalize :: (Floating t) => V3 t -> V3 t
+normalize :: V3 -> V3
 normalize (V3 x y z) = (V3 x y z) `vdiv` vlength (V3 x y z)
 
