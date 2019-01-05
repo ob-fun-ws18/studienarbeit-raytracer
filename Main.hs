93017738d445df0f4e7e20534279dee0b1bccc38
Michael Eggers <eggers@hm.edu>
2019-01-06 00:18:27 +0100

    output to ppm file

--- Main.hs
+++ Main.hs
@@ -19,9 +19,9 @@ hitSphere rayOrigin rayDir sphere radius =
           root1 = (-b-droot) / 2.0    
 
 -- camera setup
-cameraPos = (V3 5 0 5)
+cameraPos = (V3 0 0 5)
 up = (V3 0 1 0)
-lookAt = (V3 5 0 0)
+lookAt = (V3 0 0 0)
 viewDir = normalize $ lookAt `vsub` cameraPos
 viewUp = normalize $ up `vsub` ((dot up viewDir) `vmult` viewDir )
 viewRight = viewDir `cross` viewUp
@@ -29,8 +29,8 @@ viewRight = viewDir `cross` viewUp
 -- viewport setup
 viewportWidth = 2.0
 viewportHeight = 2.0
-resX = 21
-resY = 21
+resX = 1024
+resY = 1024
 pixelWidth = viewportWidth / resX
 pixelHeight = viewportHeight / resY
 stepX = pixelWidth / 2.0
@@ -52,23 +52,40 @@ cmpRay vpX vpY =
    where
        originToCenter = cameraPos `vadd` viewDir
        centerToPixel = originToCenter `vadd` (vpX `vmult` viewRight) `vadd` ((-vpY) `vmult` viewUp)
+
+-- Sphere : x,y,z pos & radius
+type Sphere = (V3, Float)
         
 -- returns distance from camera to hit or -1, if no hit
-trace :: Float -> Float -> Float
-trace gridX gridY =
-    hitSphere cameraPos rayDir (V3 0 0 0) 2
+trace :: Float -> Float -> V3 -> Float -> Float
+trace gridX gridY sphere radius =
+    hitSphere cameraPos rayDir sphere radius
     where
         rayDir = cmpRay (x(cmpVPpxl gridX gridY)) (y(cmpVPpxl gridX gridY))
        
        
-distanceList = [trace x y | x <- [0..(resX-1)], y <- [0..(resY-1)]]
+distanceList = [trace x y (V3 0 0 0) 2 | x <- [0..(resX-1)], y <- [0..(resY-1)]]
 
 -- maps distances to RGB white tuple or RGB black tuples according to distance
-toRGB888 = map (\value -> if (value > -1.0) then (255, 255, 255) else (0, 0, 0)) distanceList
+toRGBTupleList :: [(Float, Float, Float)]
+toRGBTupleList = map (\value -> if (value > -1.0) then (1.0, 1.0, 1.0) else (0.0, 0.0, 0.0)) distanceList
 
-toChar = map (\value -> if (value > -1.0) then "#" else "_") distanceList
+-- maps distances to Characters, '#' if hit, '_' otherwise
+toStringList :: [String]
+toStringList = map (\value -> if (value > -1.0) then "#" else "_") distanceList
 
--- yeah...
+-- yeah... does stuff I cannot read anymore and I forgot how it works
 printGrid arr = mapM_ (putStrLn . unwords) $ map (map show) $ chunksOf 21 arr
 
-main = printGrid toChar
+-- convert color values to PPM format
+makePPM :: Int -> Int-> [ (Float, Float, Float) ] -> String
+makePPM width height xs = "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n" ++ stringify(xs)
+		  where stringify [] = ""
+			stringify ((r,g,b):xs) = show (round (r*255)) ++ " " 
+						 ++ show (round (g*255)) ++ " " 
+						 ++ show (round (b*255)) ++ " "
+						 ++ stringify xs
+         
+
+-- main = make_pgm resX resY toRGBTupleList
+main = do writeFile "test.ppm" (makePPM (round resX) (round resY) toRGBTupleList)
