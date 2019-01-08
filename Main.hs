6952145b834a61a531ae96b1f80143bf48618844
Eggers <hm-eggers@hm.edu>
2019-01-08 17:42:10 +0100

    everything works (reflective spheres, random sphere gen)

--- Main.hs
+++ Main.hs
@@ -2,63 +2,88 @@ import System.IO
 import Control.Monad
 import Data.List.Split
 import Data.Maybe
+import System.Random
 
 import Vector
 
 data Sphere = Sphere V3 Float V3 -- Position, Radius, Color
-data HitRecord = HitRecord Float V3 V3 V3 -- Distance, Hitpoint, Normal, Color
-minimumHitRecord :: [HitRecord] -> Maybe HitRecord
+data HitRecord = HitRecord { distance :: Float, hitpoint :: V3, normal :: V3, colorOf :: V3 } -- Distance, Hitpoint, Normal, Color
+minimumHitRecord :: [Maybe HitRecord] -> Maybe HitRecord
 minimumHitRecord [] = Nothing
-minimumHitRecord [hitrec] = Just hitrec
-minimumHitRecord (x:xs) = if x `smallerThan` (minimumHitRecord xs) then Just x
+minimumHitRecord [Nothing] = Nothing
+minimumHitRecord [Just a] = Just a
+minimumHitRecord (Just x:xs) = if x `smallerThan` (minimumHitRecord xs) then Just x
                           else minimumHitRecord xs
+minimumHitRecord (Nothing:xs) = minimumHitRecord xs
 
 smallerThan :: HitRecord -> Maybe HitRecord -> Bool
 smallerThan _ Nothing = True
 smallerThan (HitRecord distance _ _ _) (Just (HitRecord distance2 _ _ _)) = distance < distance2
 
-distanceGreaterZero :: HitRecord -> Bool
-distanceGreaterZero (HitRecord distance _ _ _) = distance > 0.0
+distanceGreaterZero :: Maybe HitRecord -> Bool
+distanceGreaterZero (Just (HitRecord distance _ _ _)) = distance > 0.0
+distanceGreaterZero Nothing = False
 
 distanceOfHitrecord :: Maybe HitRecord -> Maybe Float
 distanceOfHitrecord Nothing = Nothing
 distanceOfHitrecord (Just (HitRecord distance _ _ _)) = Just distance
                
-colorOf :: Maybe HitRecord -> Maybe (Float, Float, Float)
-colorOf Nothing = Nothing
-colorOf (Just (HitRecord _ _ _ color)) = (Just (x(color), y(color), z(color)))
+-- colorOf :: Maybe HitRecord -> Maybe (Float, Float, Float)
+-- colorOf Nothing = Nothing
+-- colorOf (Just (HitRecord _ _ _ color)) = (Just (x(color), y(color), z(color)))
 
-hitSphere :: V3 -> V3 -> Sphere -> HitRecord
+data HasHit = HasHit Bool HitRecord
+
+hitSphere :: V3 -> V3 -> Sphere -> Maybe HitRecord
 hitSphere rayOrigin rayDir (Sphere pos radius color) =
     if (discriminant < 0)
-        then (HitRecord 0.0 (V3 0 0 0) (V3 0 0 0) color) -- no hit
+        then Nothing -- no hit
     else
         if root1 > 0.0
-            then (HitRecord root1 hitPoint1 normal1 color)
-        else (HitRecord ((-b+droot) / 2.0) hitPoint2 normal1 color)
+            then Just (HitRecord root1 hitPoint1 normal1 color)
+        else  Just (HitRecord ((-b+droot) / 2.0) hitPoint2 normal1 color)
     where sphereToOrigin = rayOrigin `vsub` pos
           b = 2 * (rayDir `dot` sphereToOrigin)
           c = (sphereToOrigin `dot ` sphereToOrigin) - (radius * radius)
           discriminant = b*b - 4*c
           droot = sqrt discriminant
           root1 = (-b-droot) / 2.0
-          hitPoint1 = rayOrigin `vadd` (root1 `vmult` rayDir)
-          hitPoint2 = rayOrigin `vadd` (droot `vmult` rayDir)
+          hitPoint1 = rayOrigin `vadd` ((root1-0.001) `smult` rayDir)
+          hitPoint2 = rayOrigin `vadd` ((droot-0.001) `smult` rayDir)
           normal1 = hitPoint1 `vsub` pos
           normal2 = hitPoint2 `vsub` pos
 
--- Generic hit function working on a list of hitable items (for now only Spheres)        
+    
+
+reflect :: V3 -> V3 -> V3
+reflect rayDirection normal = (-2) `smult` ((rayDirection `dot` normal) `smult` normal) `vadd` rayDirection
+    
+
+    --- Generic hit function working on a list of hitable items (for now only Spheres)
 hit :: V3 -> V3 -> [Sphere] -> Maybe HitRecord
 hit rayOrigin rayDir spheres =
     minimumHitRecord hitRecords
     where hitRecords = [ hitRecord | hitRecord <- [hitSphere rayOrigin rayDir sphere | sphere <- spheres], distanceGreaterZero hitRecord]
 
+attenuate :: (Float, Float, Float) -> (Float,Float,Float) -> (Float,Float,Float)
+attenuate (a,b,c) (x,y,z) = (a*x, b*y, c*z)
+
+color :: V3 -> V3 -> [Sphere] -> Int -> (Float, Float, Float)
+color _ _ _ 0 = (0.0,0.0,0.0)
+color rayOrigin rayDirection spheres depth = 
+    if isJust maybeHitRecord then
+        attenuate (v3ToTuple . colorOf . fromJust $ maybeHitRecord) (color (hitpoint (fromJust maybeHitRecord)) (normalize (reflect rayDirection (normal (fromJust maybeHitRecord)))) spheres (depth-1))
+    else (1,0.5*y(rayDirection)+0.5,1)
+    where
+        maybeHitRecord = hit rayOrigin rayDirection spheres
+
+
 -- camera setup
-cameraPos = (V3 0 0 5)
+cameraPos = (V3 0 0 20)
 up = (V3 0 1 0)
 lookAt = (V3 0 0 0)
 viewDir = normalize $ lookAt `vsub` cameraPos
-viewUp = normalize $ up `vsub` ((dot up viewDir) `vmult` viewDir )
+viewUp = normalize $ up `vsub` ((dot up viewDir) `smult` viewDir )
 viewRight = viewDir `cross` viewUp
 
 -- viewport setup
@@ -86,27 +111,36 @@ cmpRay vpX vpY =
    normalize $ centerToPixel `vsub` cameraPos
    where
        originToCenter = cameraPos `vadd` viewDir
-       centerToPixel = originToCenter `vadd` (vpX `vmult` viewRight) `vadd` ((-vpY) `vmult` viewUp)
+       centerToPixel = originToCenter `vadd` (vpX `smult` viewRight) `vadd` ((-vpY) `smult` viewUp)
         
 -- returns distance from camera to hit or -1, if no hit
-trace :: Float -> Float -> [Sphere] -> Maybe HitRecord
+trace :: Float -> Float -> [Sphere] -> (Float, Float, Float)
 trace gridX gridY sphereList =
-    hitrecord
+    color cameraPos rayDir sphereList 100
     where
         rayDir = cmpRay (x(cmpVPpxl gridX gridY)) (y(cmpVPpxl gridX gridY))
-        hitrecord = hit cameraPos rayDir sphereList
        
        
-hitRecordList = [trace x y [(Sphere (V3 (-1) 0 0) 1 (V3 1 0 0)), (Sphere (V3 0.5 0 0) 1 (V3 0 1 0))] | x <- [0..(resX-1)], y <- [0..(resY-1)]]
-
--- maps distances to RGB white tuple or RGB black tuples according to distance
-toRGBTupleList :: [Maybe HitRecord] -> [(Float, Float, Float)]
--- toRGBTupleList hitrecordList = [color | color <- [hitrec | hitrec <- colorOf hitrecordList, if isJust hitrec then (colorOf hitrec) else (0,0,0)]]
-toRGBTupleList hitrecordList = map hitrecordToColor hitrecordList
-
-hitrecordToColor Nothing = (0,0,0)
-hitrecordToColor (Just (HitRecord _ _ _ (V3 x y z))) = (x, y, z)
-
+randomSphereList :: Int -> IO [Sphere]
+randomSphereList sphereCount = forM [1..sphereCount] $ \_ -> makeRandomSphere
+    
+makeRandomSphere = do
+    colorR <- randomRIO (0.1, 1.0)
+    colorG <- randomRIO (0.1, 1.0)
+    colorB <- randomRIO (0.1, 1.0)
+    
+    radius <- randomRIO (0.3, 2.0)
+    
+    posX <- randomRIO (-10.0, 10.0)
+    posY <- randomRIO (-10.0, 10.0)
+    posZ <- randomRIO (-10.0, 10.0)
+    
+    return $ Sphere (V3 posX posY posZ) radius (V3 colorR colorG colorB)
+        
+defaultSpheres = [(Sphere (V3 (-1) 0 0) 1 (V3 1 1 1)), (Sphere (V3 1 0 0) 1 (V3 0.2 1 1))]
+       
+       
+colors spheres = [trace x y spheres | x <- [0..(resX-1)], y <- [0..(resY-1)]]
 
 -- maps distances to Characters, '#' if hit, '_' otherwise
 -- toStringList :: [String]
@@ -126,4 +160,6 @@ makePPM width height xs = "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n
          
 
 -- main = make_pgm resX resY toRGBTupleList
-main = do writeFile "test.ppm" (makePPM (round resX) (round resY) (toRGBTupleList hitRecordList))
+main = do 
+    spheres <- randomSphereList 60
+    writeFile "test.ppm" (makePPM (round resX) (round resY) (colors spheres))
--- Vector.hs
+++ Vector.hs
@@ -3,12 +3,14 @@ module Vector
     V3(..),
     vadd,
     vsub,
-    vmult,
+    -- vmult,
+    smult,
     vdiv,
     dot,
     vlength,
     normalize,
-    cross
+    cross,
+    v3ToTuple
 ) where
 
 data V3 = V3 { x :: Float, y :: Float, z :: Float } deriving (Show)  
@@ -19,8 +21,9 @@ vadd :: V3 -> V3 -> V3
 vsub :: V3 -> V3 -> V3
 (V3 x y z) `vsub` (V3 x2 y2 z2) = V3 (x-x2) (y-y2) (z-z2)
 
-vmult :: Float -> V3 -> V3
-(s) `vmult` (V3 x y z) = V3 (s*x) (s*y) (s*z)
+smult :: Float -> V3 -> V3
+(s) `smult` (V3 x y z) = V3 (s*x) (s*y) (s*z)
+
 
 vdiv ::  V3 -> Float -> V3
 (V3 x y z) `vdiv` (s) = V3 (x/s) (y/s) (z/s)
@@ -36,4 +39,7 @@ normalize (V3 x y z) = (V3 x y z) `vdiv` vlength (V3 x y z)
 
 cross :: V3 -> V3 -> V3
 (V3 x y z) `cross` (V3 x2 y2 z2) =  V3 (y*z2 - z*y2) (z*x2 - x*z2) (x*y2 - y*x2)
+
+v3ToTuple :: V3 -> (Float, Float, Float)
+v3ToTuple vec = (x vec, y vec, z vec)
 
